309bccbd554ff4c1c4264541e7507ec9f71eac47
NatalyIvanova <letter4natalia@gmail.com>
2021-05-04 23:30:59 +0300

    color-palette start

---
+++ src/stories/colors/color-palette.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'p-colors',
+  templateUrl: './color-palette.component.html',
+  styleUrls: ['./color-palette.component.scss'],
+})
+export default class ColorPaletteComponent {
+
+}
