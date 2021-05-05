084c20e138badc0f83f030f7fdfdf901fd52b10c
NatalyIvanova <letter4natalia@gmail.com>
2021-05-05 22:48:49 +0300

    color page completed

--- src/stories/colors/color-palette.component.ts
+++ src/stories/colors/color-palette.component.ts
@@ -3,22 +3,63 @@ import {KeyValue} from '@angular/common';
 
 const palette = {
   primary: {
-    basic: { main: 'P-A 900', contrast: 'P-C 50' },
-    accent: { main: 'P-B 300' },
+    basic: {
+      contrast: {name: 'P-C 50', labelColor: 'black'},
+      main: {name: 'P-A 900', labelColor: 'white'},
+    },
+    accent: {
+      main: { name: 'P-A 300', labelColor: 'white' }
+    },
   },
   secondary: {
-    basic: { 800: 'S-A 800', 700: 'S-A 700', 600: 'S-A 600', 500: 'S-A 500', 200: 'S-A 200', 100: 'S-A 100', 50: 'S-A 50' },
-    accent: { 700: 'S-B 700', 500: 'S-B 500', 400: 'S-B 400', 300: 'S-B 300', 200: 'S-B 200', 100: 'S-B 100', 50: 'S-B 500' }
+    basic: { 800: { name: 'S-A 800', labelColor: 'white' },
+      700: { name: 'S-A 700', labelColor: 'white' },
+      600: { name: 'S-A 600', labelColor: 'white' },
+      500: { name: 'S-A 500', labelColor: 'black' },
+      200: { name: 'S-A 200', labelColor: 'black' },
+      100: { name: 'S-A 100', labelColor: 'black' },
+      50: { name: 'S-A 50', labelColor: 'black' } },
+    accent: { 700: { name: 'S-B 700', labelColor: 'white' },
+      500: { name: 'S-B 500', labelColor: 'white' },
+      400: { name: 'S-B 400', labelColor: 'white' },
+      300: { name: 'S-B 300', labelColor: 'white' },
+      200: { name: 'S-B 200', labelColor: 'black' },
+      100: { name: 'S-B 100', labelColor: 'black' },
+      50: { name: 'S-B 500', labelColor: 'black' }
+    }
   },
   gray: {
-    scale: { 800: 'grayscale 800', 700: 'grayscale 700', 600: 'grayscale 600', 500: 'grayscale 500', 400: 'grayscale 400', 100: 'grayscale 100', 50: 'grayscale 50', 5: 'grayscale 5', 1: 'grayscale 1' }
+    scale: { 800: { name: 'grayscale 800', labelColor: 'white' },
+        700: { name: 'grayscale 700', labelColor: 'white' },
+        600: { name: 'grayscale 600', labelColor: 'white' },
+        500: { name: 'grayscale 500', labelColor: 'white' },
+        400: { name: 'grayscale 400', labelColor: 'black' },
+        100: { name: 'grayscale 100', labelColor: 'black' },
+        50: { name: 'grayscale 50', labelColor: 'black' },
+        5: { name: 'grayscale 5', labelColor: 'black' },
+        1: { name: 'grayscale 1', labelColor: 'black' }
+    }
   },
   supportive: {
-    alert: { 700: 'Alert 700', 500: 'Alert 500', 50: 'Alert 50' },
-    yellow: { 300: 'Yellow 300' },
-    orange: { 300: 'Orange 300', 50: 'Orange 50' },
-    success: { 400: 'Success 400', 50: 'Success 50' },
-    dev: { a: 'Dev A', b: 'Dev B', c: 'Dev C' }
+    alert: {
+      700: { name: 'Alert 700', labelColor: 'white' },
+      500: { name: 'Alert 500', labelColor: 'black' },
+      50: { name: 'Alert 50', labelColor: 'black' }
+    },
+    yellow: { 300: { name: 'Yellow 300', labelColor: 'black' } },
+    orange: {
+      300: { name: 'Orange 300', labelColor: 'black' },
+      50: { name: 'Orange 50', labelColor: 'black' }
+    },
+    success: {
+      400: { name: 'Success 400', labelColor: 'black' },
+      50: { name: 'Success 50', labelColor: 'black' }
+    },
+    dev: {
+      a: { name: 'Dev A', labelColor: 'white' },
+      b: { name: 'Dev B', labelColor: 'white' },
+      c: { name: 'Dev C', labelColor: 'white' }
+    }
   }
 };
 
@@ -33,4 +74,9 @@ export default class ColorPaletteComponent {
   originalOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
     return 0;
   }
+
+  reverseKeyOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
+    return -1;
+  }
+
 }
