5e2b91e3cca1d3e70c8f5af32fdb7ad0d1b48d93
NatalyIvanova <letter4natalia@gmail.com>
2021-05-11 13:50:51 +0300

    button-page completed (no icons)

--- src/stories/button/button-page.component.ts
+++ src/stories/button/button-page.component.ts
@@ -2,13 +2,34 @@ import {Component, Input} from '@angular/core';
 import IButton from './button.interface';
 import {KeyValue} from '@angular/common';
 
+interface IButtonsPage {
+  small: {
+    height: string;
+    buttons: {
+      primary: IButton[];
+      secondary: IButton[];
+      tertiary: IButton[];
+      distructive: IButton[]
+    }
+  };
+  large: {
+    height: string;
+    buttons: {
+      primary: IButton[];
+      secondary: IButton[];
+      tertiary: IButton[];
+      distructive: IButton[]
+    }
+  };
+}
+
 @Component({
   selector: 'p-button-page',
   templateUrl: './button-page.component.html',
   styleUrls: ['./button-page.component.scss'],
 })
 export default class ButtonPageComponent {
-  @Input() buttons: IButton[] = [];
+  @Input() buttonsPage: IButtonsPage;
 
   originalOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
     return 0;
