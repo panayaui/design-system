381d1ba6b8187d05979223644629b6dfa090ddb4
NatalyIvanova <letter4natalia@gmail.com>
2021-05-11 10:41:32 +0300

    button with fake icons

--- src/stories/button/button-page.stories.ts
+++ src/stories/button/button-page.stories.ts
@@ -1,22 +1,22 @@
-import { moduleMetadata } from '@storybook/angular';
-import { CommonModule } from '@angular/common';
-import { Story, Meta } from '@storybook/angular/types-6-0';
-import ButtonPageComponent from './button-page.component';
-
-export default {
-  title: 'Components/Buttons',
-  component: ButtonPageComponent,
-  decorators: [
-    moduleMetadata({
-      declarations: [ButtonPageComponent],
-      imports: [CommonModule],
-    }),
-  ],
-} as Meta;
-
-const Template: Story<ButtonPageComponent> = (args: ButtonPageComponent) => ({
-  props: args,
-});
-
-export const Buttons = Template.bind({});
-Buttons.args = {};
+// import { moduleMetadata } from '@storybook/angular';
+// import { CommonModule } from '@angular/common';
+// import { Story, Meta } from '@storybook/angular/types-6-0';
+// import ButtonPageComponent from './button-page.component';
+//
+// export default {
+//   title: 'Components/Buttons',
+//   component: ButtonPageComponent,
+//   decorators: [
+//     moduleMetadata({
+//       declarations: [ButtonPageComponent],
+//       imports: [CommonModule],
+//     }),
+//   ],
+// } as Meta;
+//
+// const Template: Story<ButtonPageComponent> = (args: ButtonPageComponent) => ({
+//   props: args,
+// });
+//
+// export const Buttons = Template.bind({});
+// Buttons.args = {};
