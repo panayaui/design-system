1dd144102249737d27b81b4e7e27de7f45e77b3d
NatalyIvanova <letter4natalia@gmail.com>
2021-05-12 13:41:30 +0300

    button & button-page: flat params + disabled

--- src/stories/button/button-component/button.component.ts
+++ src/stories/button/button-component/button.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import IButton from '../button.interface';
+import {ButtonTypeEnum} from '../button-type.enum';
 
 @Component({
   selector: 'p-button',
@@ -7,15 +8,20 @@ import IButton from '../button.interface';
   styleUrls: ['./button.component.scss'],
 })
 export default class ButtonComponent {
-  @Input() button: IButton;
+  @Input() label: string = 'Label';
+  @Input() large: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() iconBefore: string = 'view';
+  @Input() iconAfter: string = 'arrow';
+  @Input() buttonType: ButtonTypeEnum = ButtonTypeEnum.Primary;
 
   public get classes(): string[] {
+    console.log(this);
     const classParams = [];
-    this.button.large ? classParams.push('p-btn-large') : classParams.push('p-btn-small');
-    this.button.disabled ? classParams.push('p-btn-disabled') : null;
-    this.button.iconBefore ? classParams.push('p-btn-icon-before') : null;
-    this.button.iconAfter ? classParams.push('p-btn-icon-after') : null;
-    classParams.push(`p-btn-${this.button.buttonType}`);
+    this.large ? classParams.push('p-btn-large') : classParams.push('p-btn-small');
+    this.iconBefore ? classParams.push('p-btn-icon-before') : null;
+    this.iconAfter ? classParams.push('p-btn-icon-after') : null;
+    classParams.push(`p-btn-${this.buttonType}`);
     return classParams;
   }
 }
