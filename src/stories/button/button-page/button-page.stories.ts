ebe7f2b972d523bcfa27371e69d962f788b17fe7
NatalyIvanova <nataliai@netcraft.co.il>
2021-05-24 18:22:16 +0300

    buttons page: new layout

--- src/stories/button/button-page/button-page.stories.ts
+++ src/stories/button/button-page/button-page.stories.ts
@@ -32,29 +32,41 @@ Buttons.args = {
           {...ButtonStories.Primary.args},
           {...ButtonStories.Primary.args, disabled: true},
           {...ButtonStories.Primary.args, iconBefore: 'eye'},
+          {...ButtonStories.Primary.args, iconBefore: 'eye', disabled: true},
           {...ButtonStories.Primary.args, iconAfter: 'chevron-down'},
-          {...ButtonStories.Primary.args, iconBefore: 'eye', iconAfter: 'chevron-down'}
+          {...ButtonStories.Primary.args, iconAfter: 'chevron-down', disabled: true},
+          {...ButtonStories.Primary.args, iconBefore: 'eye', iconAfter: 'chevron-down'},
+          {...ButtonStories.Primary.args, iconBefore: 'eye', iconAfter: 'chevron-down', disabled: true}
         ],
         secondary: [
           {...ButtonStories.Secondary.args},
           {...ButtonStories.Secondary.args, disabled: true},
           {...ButtonStories.Secondary.args, iconBefore: 'eye'},
+          {...ButtonStories.Secondary.args, iconBefore: 'eye', disabled: true},
           {...ButtonStories.Secondary.args, iconAfter: 'chevron-down'},
-          {...ButtonStories.Secondary.args, iconBefore: 'eye', iconAfter: 'chevron-down'}
+          {...ButtonStories.Secondary.args, iconAfter: 'chevron-down', disabled: true},
+          {...ButtonStories.Secondary.args, iconBefore: 'eye', iconAfter: 'chevron-down'},
+          {...ButtonStories.Secondary.args, iconBefore: 'eye', iconAfter: 'chevron-down', disabled: true}
         ],
         tertiary: [
           {...ButtonStories.Tertiary.args},
           {...ButtonStories.Tertiary.args, disabled: true},
           {...ButtonStories.Tertiary.args, iconBefore: 'eye'},
+          {...ButtonStories.Tertiary.args, iconBefore: 'eye', disabled: true},
           {...ButtonStories.Tertiary.args, iconAfter: 'chevron-down'},
-          {...ButtonStories.Tertiary.args, iconBefore: 'eye', iconAfter: 'chevron-down'}
+          {...ButtonStories.Tertiary.args, iconAfter: 'chevron-down', disabled: true},
+          {...ButtonStories.Tertiary.args, iconBefore: 'eye', iconAfter: 'chevron-down'},
+          {...ButtonStories.Tertiary.args, iconBefore: 'eye', iconAfter: 'chevron-down', disabled: true}
         ],
         distructive: [
           {...ButtonStories.Distructive.args},
           {...ButtonStories.Distructive.args, disabled: true},
           {...ButtonStories.Distructive.args, iconBefore: 'eye'},
+          {...ButtonStories.Distructive.args, iconBefore: 'eye', disabled: true},
           {...ButtonStories.Distructive.args, iconAfter: 'chevron-down'},
-          {...ButtonStories.Distructive.args, iconBefore: 'eye', iconAfter: 'chevron-down'}
+          {...ButtonStories.Distructive.args, iconAfter: 'chevron-down', disabled: true},
+          {...ButtonStories.Distructive.args, iconBefore: 'eye', iconAfter: 'chevron-down'},
+          {...ButtonStories.Distructive.args, iconBefore: 'eye', iconAfter: 'chevron-down', disabled: true}
         ]
       }
     },
@@ -65,32 +77,43 @@ Buttons.args = {
           {...ButtonStories.Primary.args, large: true},
           {...ButtonStories.Primary.args, large: true, disabled: true},
           {...ButtonStories.Primary.args, large: true, iconBefore: 'eye'},
+          {...ButtonStories.Primary.args, large: true, iconBefore: 'eye', disabled: true},
           {...ButtonStories.Primary.args, large: true, iconAfter: 'chevron-down'},
-          {...ButtonStories.Primary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down'}
+          {...ButtonStories.Primary.args, large: true, iconAfter: 'chevron-down', disabled: true},
+          {...ButtonStories.Primary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down'},
+          {...ButtonStories.Primary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down', disabled: true}
         ],
         secondary: [
           {...ButtonStories.Secondary.args, large: true},
           {...ButtonStories.Secondary.args, large: true, disabled: true},
           {...ButtonStories.Secondary.args, large: true, iconBefore: 'eye'},
+          {...ButtonStories.Secondary.args, large: true, iconBefore: 'eye', disabled: true},
           {...ButtonStories.Secondary.args, large: true, iconAfter: 'chevron-down'},
-          {...ButtonStories.Secondary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down'}
+          {...ButtonStories.Secondary.args, large: true, iconAfter: 'chevron-down', disabled: true},
+          {...ButtonStories.Secondary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down'},
+          {...ButtonStories.Secondary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down', disabled: true}
         ],
         tertiary: [
           {...ButtonStories.Tertiary.args, large: true},
           {...ButtonStories.Tertiary.args, large: true, disabled: true},
           {...ButtonStories.Tertiary.args, large: true, iconBefore: 'eye'},
+          {...ButtonStories.Tertiary.args, large: true, iconBefore: 'eye', disabled: true},
           {...ButtonStories.Tertiary.args, large: true, iconAfter: 'chevron-down'},
-          {...ButtonStories.Tertiary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down'}
+          {...ButtonStories.Tertiary.args, large: true, iconAfter: 'chevron-down', disabled: true},
+          {...ButtonStories.Tertiary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down'},
+          {...ButtonStories.Tertiary.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down', disabled: true}
         ],
         distructive: [
           {...ButtonStories.Distructive.args, large: true},
           {...ButtonStories.Distructive.args, large: true, disabled: true},
           {...ButtonStories.Distructive.args, large: true, iconBefore: 'eye'},
+          {...ButtonStories.Distructive.args, large: true, iconBefore: 'eye', disabled: true},
           {...ButtonStories.Distructive.args, large: true, iconAfter: 'chevron-down'},
-          {...ButtonStories.Distructive.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down'}
+          {...ButtonStories.Distructive.args, large: true, iconAfter: 'chevron-down', disabled: true},
+          {...ButtonStories.Distructive.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down'},
+          {...ButtonStories.Distructive.args, large: true, iconBefore: 'eye', iconAfter: 'chevron-down', disabled: true}
         ]
       }
-
     }
   }
 };
