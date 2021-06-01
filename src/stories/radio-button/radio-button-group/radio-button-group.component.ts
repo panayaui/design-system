a769a719ee883f8b883701dfbd9197d475df0fa3
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-01 21:47:39 +0300

    radio-button-group start

---
+++ src/stories/radio-button/radio-button-group/radio-button-group.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, OnInit} from '@angular/core';
+import IRadioButton from '../radio-button.interface';
+import {FormControl, ValidatorFn} from '@angular/forms';
+
+@Component({
+  selector: 'p-radio-group',
+  templateUrl: './radio-button-group.component.html',
+  styleUrls: ['./radio-button-group.component.scss'],
+})
+export default class RadioButtonGroup implements OnInit {
+  @Input() label: string;
+  @Input() optionsList: IRadioButton[];
+  @Input() errorMessage: string;
+  @Input() value: any;
+  @Input() selected: any;
+  @Input() disabled: boolean = false;
+  @Input() required: boolean = false;
+  @Input() validators: ValidatorFn[];
+  public fieldFormControl: FormControl;
+
+  ngOnInit(): void {
+    this.fieldFormControl = new FormControl(
+      {value: this.value, disabled: this.disabled},
+      this.validators);
+  }
+}
