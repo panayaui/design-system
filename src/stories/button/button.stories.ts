8334247ff8e3bcc52536efae10d3ba791811fcba
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-03 23:15:25 +0300

    buttons with icons in mdx

--- src/stories/button/button.stories.ts
+++ src/stories/button/button.stories.ts
@@ -3,17 +3,16 @@ import {CommonModule} from '@angular/common';
 import {Story, Meta} from '@storybook/angular/types-6-0';
 // import {ButtonComponent} from 'design-system-lib';
 // import {ButtonTypeEnum} from 'design-system-lib';
-import {ButtonComponent} from '../../../projects/design-system-lib/src/lib/button/button.component';
-import {IconModule} from '../../../projects/design-system-lib/src/lib/icon/icon.module';
-import {ButtonTypeEnum} from '../../../projects/design-system-lib/src/lib/button/button-type.enum';
+import {ButtonComponent} from 'projects/design-system-lib/src/lib/button/button.component';
+import {IconModule} from 'projects/design-system-lib/src/lib/icon/icon.module';
+import {ButtonTypeEnum} from 'projects/design-system-lib/src/lib/button/button-type.enum';
 
 export default {
   title: 'Button',
   component: ButtonComponent,
   decorators: [
     moduleMetadata({
-      declarations: [ButtonComponent],
-      imports: [CommonModule, IconModule],
+      imports: [IconModule],
     }),
     componentWrapperDecorator((story) => `<div style="margin: 3em">${story}</div>`)
   ],
