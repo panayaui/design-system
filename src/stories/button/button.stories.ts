b8545b3bc14feebd943192c0b0dcd2163a0d25b4
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-03 22:33:00 +0300

    stories: no import from the lib

--- src/stories/button/button.stories.ts
+++ src/stories/button/button.stories.ts
@@ -1,6 +1,8 @@
 import {componentWrapperDecorator, moduleMetadata} from '@storybook/angular';
 import {CommonModule} from '@angular/common';
 import {Story, Meta} from '@storybook/angular/types-6-0';
+// import {ButtonComponent} from 'design-system-lib';
+// import {ButtonTypeEnum} from 'design-system-lib';
 import {ButtonComponent} from '../../../projects/design-system-lib/src/lib/button/button.component';
 import {IconModule} from '../../../projects/design-system-lib/src/lib/icon/icon.module';
 import {ButtonTypeEnum} from '../../../projects/design-system-lib/src/lib/button/button-type.enum';
