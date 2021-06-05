781f990440000f3e5f75b9d98a65ad27128d7e68
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-05 22:32:39 +0300

    all button with icons stories are added

--- src/stories/icons/icon.stories.ts
+++ src/stories/icons/icon.stories.ts
@@ -3,15 +3,15 @@ import {Story, Meta} from '@storybook/angular/types-6-0';
 import {IconComponent} from '../../../projects/design-system-lib/src/lib/icon/icon.component';
 import {IconModule} from '../../../projects/design-system-lib/src/lib/icon/icon.module';
 
-export default {
-  title: 'Icons',
-  component: IconComponent,
-  decorators: [
-    moduleMetadata({
-      imports: [IconModule],
-    }),
-  ],
-} as Meta;
+// export default {
+//   title: 'Icons',
+//   component: IconComponent,
+//   decorators: [
+//     moduleMetadata({
+//       imports: [IconModule],
+//     }),
+//   ],
+// } as Meta;
 
 const Template: Story<IconComponent> = (args: IconComponent) => ({
   props: args,
