e6bde5e17168e09c8b60f661b6dd858ee61d302f
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-06 12:51:00 +0300

    label-main moved to the lib

--- projects/design-system-lib/src/public-api.ts
+++ projects/design-system-lib/src/public-api.ts
@@ -1,10 +1,10 @@
-/*
- * Public API Surface of design-system-lib
- */
-
-
 export * from './lib/button/button.module';
 export * from './lib/button/button.component';
 export * from './lib/button/button-type.enum';
-export * from './lib/icon/icon.component';
+
 export * from './lib/icon/icon.module';
+export * from './lib/icon/icon.component';
+
+export * from './lib/label-main/label-main.module';
+export * from './lib/label-main/label-main.component';
+export * from './lib/label-main/label-main.enum';
