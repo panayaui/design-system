fecf767a2be05f5f05f8b3d62e4ac6c615f8c317
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-06 23:07:08 +0300

    toggle button with label - styles completed

--- projects/design-system-lib/src/lib/toggle/toggle.component.ts
+++ projects/design-system-lib/src/lib/toggle/toggle.component.ts
@@ -1,5 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {Component} from '@angular/core';
 
 
 @Component({
