9993fb052f1670a022fe7af4d5899f0039fee6f9
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-14 19:07:01 +0300

    menu pixel pushing

--- src/stories/menu/stateGroups.ts
+++ src/stories/menu/stateGroups.ts
@@ -16,4 +16,25 @@ export const stateGroups = [{
 }, {
   letter: 'States H',
   names: [{name: 'Hawaii'}]
-}];
+}, {
+  letter: 'I',
+  names: [{name: 'Idaho'}, {name: 'Illinois'}, {name: 'Indiana'}, {name: 'Iowa'}]
+}, {
+  letter: 'K',
+  names: [{name: 'Kansas'}, {name: 'Kentucky'}]
+}, {
+  letter: 'L',
+  names: [{name: 'Louisiana'}]
+}, {
+  letter: 'M',
+  names: [{name: 'Maine'}, {name: 'Maryland'}, {name: 'Massachusetts'}, {name: 'Michigan'},
+    {name: 'Minnesota'}, {name: 'Mississippi'}, {name: 'Missouri'}, {name: 'Montana'}]
+}, {
+  letter: 'N',
+  names: [{name: 'Nebraska'}, {name: 'Nevada'}, {name: 'New Hampshire'}, {name: 'New Jersey'},
+    {name: 'New Mexico'}, {name: 'New York'}, {name: 'North Carolina'}, {name: 'North Dakota'}]
+}, {
+  letter: 'O',
+  names: [{name: 'Ohio'}, {name: 'Oklahoma'}, {name: 'Oregon'}]
+}
+];
