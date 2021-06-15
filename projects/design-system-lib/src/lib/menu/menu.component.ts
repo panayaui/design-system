008cee0557150f4ad3771a5b7b0efa85081e027c
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-15 23:24:25 +0300

    menu with filter - header and footer

--- projects/design-system-lib/src/lib/menu/menu.component.ts
+++ projects/design-system-lib/src/lib/menu/menu.component.ts
@@ -1,6 +1,14 @@
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {FormControl, ValidatorFn} from '@angular/forms';
 import {SubscriptionLike} from 'rxjs';
+import {IIcon} from '../icon/icon.interface';
+import {ButtonTypeEnum} from '../button/button-type.enum';
+
+interface IMenuButton {
+  label: string;
+  buttonType: ButtonTypeEnum;
+  icon: IIcon;
+}
 
 @Component({
   selector: 'p-menu',
@@ -11,17 +19,25 @@ export class MenuComponent implements OnInit, OnDestroy{
   @Input() menuList: any[];
   @Input() groupTitle: boolean = false; //if there is a group title
   @Input() filter: boolean = false; // if there is a search and buttons
+  @Input() inputPlaceholder: string;
+  @Input() inputAriaLabel: string;
+  @Input() buttonFirst: IMenuButton;
+  @Input() buttonLast: IMenuButton;
   public filteredList: any[];
   public formFieldControl: FormControl;
   private sub: SubscriptionLike;
 
   ngOnInit(): void {
+    console.log(this.buttonFirst);
     this.filteredList = this.menuList;
     if (this.filter) {
       this.formFieldControl = new FormControl('');
       this.sub = this.formFieldControl.valueChanges.subscribe((value: string) => {
         this.alterList(value);
       });
+    }
+    if (!this.inputAriaLabel && this.inputPlaceholder) {
+      this.inputAriaLabel = this.inputPlaceholder;
     }
   }
 
