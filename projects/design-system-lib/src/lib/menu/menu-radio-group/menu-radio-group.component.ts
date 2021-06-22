e126155b1c99e3a12b81901d48dfa13bcd209db9
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-22 23:44:56 +0300

    menu-radio-group: styles, no calendar

--- projects/design-system-lib/src/lib/menu/menu-radio-group/menu-radio-group.component.ts
+++ projects/design-system-lib/src/lib/menu/menu-radio-group/menu-radio-group.component.ts
@@ -9,9 +9,12 @@ import {MatMenuTrigger} from '@angular/material/menu';
 import {IMenuButton} from '../menu-button.interface';
 import {IRadioButton} from '../../radio-button/radio-button.interface';
 import {MenuRadioOptionEnum} from './menu-radio-option.enum';
+import {InputNumericSizeEnum} from '../../input/numeric/input-numeric-size.enum';
 
 interface IMenuRadioOption extends IRadioButton {
   optionType: MenuRadioOptionEnum;
+  numericInputValue: number;
+  menuList: any[];
 }
 
 @Component({
@@ -24,18 +27,16 @@ export class MenuRadioGroupComponent implements OnInit {
   @Input() menuList: IMenuRadioOption[];
   @Input() footerBtnFirst: IMenuButton;
   @Input() footerBtnLast: IMenuButton;
-  @Input() groupValue: any;
-  @Input() selected: any;
-  @Input() disabled: boolean = false;
-  @Input() required: boolean = false;
-  @Input() validators: ValidatorFn[];
+
   @ViewChild('menuTrigger') menuTrigger: MatMenuTrigger;
-  public fieldFormControl: FormControl;
+  public inputNumericSize = InputNumericSizeEnum;
 
   ngOnInit(): void {
-    this.fieldFormControl = new FormControl(
-      {value: this.groupValue, disabled: this.disabled},
-      this.validators);
+
+  }
+
+  findMenuTriggerName(menuList): void {
+    return menuList.find( item => item.selected === true).name;
   }
 
   selectCompleted(): void {
