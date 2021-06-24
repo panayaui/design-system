b375a872aa65a3fa06451cfd2809d3ad7bb0521b
NatalyIvanova <nataliai@netcraft.co.il>
2021-06-24 10:55:03 +0300

    menu-free-text: submenu added

--- projects/design-system-lib/src/lib/menu/menu-free-text/menu-free-text.component.ts
+++ projects/design-system-lib/src/lib/menu/menu-free-text/menu-free-text.component.ts
@@ -1,25 +1,32 @@
 import {
   Component,
-  Input,
+  Input, OnInit,
   ViewChild
 } from '@angular/core';
 import {MatMenuTrigger} from '@angular/material/menu';
 import {IMenuButton} from '../menu-button.interface';
+
+interface IMenuFreeTextItem {
+  name: string;
+  inputValue?: string;
+  selected?: boolean;
+}
 
 @Component({
   selector: 'p-menu-free-text',
   templateUrl: './menu-free-text.component.html',
   styleUrls: ['./menu-free-text.component.scss'],
 })
-export class MenuFreeTextComponent {
+export class MenuFreeTextComponent implements OnInit{
   @Input() menuTriggerName: string;
-  @Input() menuList: any[];
+  @Input() menuList: IMenuFreeTextItem[];
   @Input() footerBtnFirst: IMenuButton;
   @Input() footerBtnLast: IMenuButton;
   @ViewChild('menuTrigger') menuTrigger: MatMenuTrigger;
+  public subMenuTriggerName: string;
 
-  findMenuTriggerName(menuList): void {
-    return menuList.find( item => item.selected === true).name;
+  ngOnInit(): void {
+    this.subMenuTriggerName = this.menuList.find( item => item.selected).name;
   }
 
   selectCompleted(): void {
