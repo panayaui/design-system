3e7262a31ddc8d0c0e5d656ca0382079f9e2dfef
NatalyIvanova <nataliai@netcraft.co.il>
2021-07-12 10:15:46 +0300

    tooltip with arrow

--- projects/design-system-lib/src/lib/chips/chips.module.ts
+++ projects/design-system-lib/src/lib/chips/chips.module.ts
@@ -5,10 +5,12 @@ import {ChipsComponent} from './bar/chips.component';
 import {MatChipsModule} from '@angular/material/chips';
 import {ChipButtonComponent} from './button/chip-button.component';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserAnimationsModule,
     MatChipsModule,
     MatTooltipModule,
     IconModule
