f12d3a9c691f8f0e109e0c7672067bce14ecb7c3
NatalyIvanova <nataliai@netcraft.co.il>
2021-07-22 17:04:57 +0300

    input text table layout - previous structure

--- projects/design-system-lib/src/lib/input/table-layout/input-text-table-layout.component.ts
+++ projects/design-system-lib/src/lib/input/table-layout/input-text-table-layout.component.ts
@@ -1,39 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {IInputTextTable} from './input-text-table.interface';
+import {Component, Input} from '@angular/core';
+import {IInputTextTableLine} from './input-text-table-line.interface';
 
 @Component({
   selector: 'p-input-text-table-layout',
   templateUrl: './input-text-table-layout.component.html',
   styleUrls: ['./input-text-table-layout.component.scss'],
 })
-export class InputTextTableLayoutComponent implements OnInit {
-  @Input() table: IInputTextTable[];
-  public tableToRender: IInputTextTable[];
-
-  ngOnInit(): void {
-    this.tableToRender = this.table.map( singleTable => {
-      return {...singleTable, isHidden: false};
-    });
-  }
-
-  toggleTable(tableInd: number): void {
-    const timer = 100;
-    let delay: number;
-    let delayStep: number;
-    this.tableToRender[tableInd].isHidden = !this.tableToRender[tableInd].isHidden;
-    if (this.tableToRender[tableInd].isHidden) {
-      delay = (this.tableToRender[tableInd].singleTableData.length + 1) * timer;
-      delayStep = timer * -1;
-    }
-    else {
-      delay = 0;
-      delayStep = timer;
-    }
-    this.tableToRender[tableInd].singleTableData.forEach( line => {
-      delay += delayStep;
-      line.transitionDelay = delay;
-    });
-  }
+export class InputTextTableLayoutComponent {
+  @Input()  title: string;
+  @Input() table: IInputTextTableLine[];
+  public isHidden: boolean = false;
 }
-
-
--- projects/design-system-lib/src/lib/input/table-layout/input-text-table.interface.ts
+++
@@ -1,7 +0,0 @@
-import {IInputTextTableLine} from './input-text-table-line.interface';
-
-export interface IInputTextTable {
-  title: string;
-  singleTableData: IInputTextTableLine[];
-  isHidden?: boolean;
-}
