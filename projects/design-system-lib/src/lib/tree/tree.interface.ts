8367f4f3fc7837abdac4782d10966f224cabccc4
NatalyIvanova <letter4natalia@gmail.com>
2021-07-26 10:45:03 +0300

    tree page fixes

--- projects/design-system-lib/src/lib/tree/tree.interface.ts
+++ projects/design-system-lib/src/lib/tree/tree.interface.ts
@@ -2,6 +2,7 @@ import {LabelTypesEnum} from '../labels/label-types.enum';
 
 export interface ITreeNode {
   name: string;
+  selected?: boolean;
   isCycle?: boolean;
   label?: LabelTypesEnum;
   children?: ITreeNode[];
