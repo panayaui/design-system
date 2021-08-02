6588b6747cc0347c964645dd8432215bc0a136c9
NatalyIvanova <letter4natalia@gmail.com>
2021-08-02 22:50:53 +0300

    tree: first item in bold

--- projects/design-system-lib/src/lib/tree/tree.interface.ts
+++ projects/design-system-lib/src/lib/tree/tree.interface.ts
@@ -2,6 +2,7 @@ import {LabelTypesEnum} from '../labels/label-types.enum';
 
 export interface ITreeNode {
   name: string;
+  isProject?: boolean;
   selected?: boolean;
   isCycle?: boolean;
   label?: LabelTypesEnum;
