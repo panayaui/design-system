f1f325d5c09a759f095fbbe8881c6755a24b7f60
NatalyIvanova <letter4natalia@gmail.com>
2021-08-02 18:05:16 +0300

    breadcrumbs completed

--- projects/design-system-lib/src/public-api.ts
+++ projects/design-system-lib/src/public-api.ts
@@ -118,3 +118,5 @@ export * from './lib/slide-toggle/slide-toggle.component';
 
 export * from './lib/masthead/masthead.module';
 export * from './lib/masthead/masthead.component';
+
+export * from './lib/breadcrumbs/breadcrumbs.component';
