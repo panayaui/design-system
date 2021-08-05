c941ed996ecc2a82ff91fe4ec778ea6930890f6f
NatalyIvanova <letter4natalia@gmail.com>
2021-08-05 12:22:25 +0300

    masthead-search start

---
+++ projects/design-system-lib/src/lib/masthead/masthead-search/masthead-search.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'p-masthead-search',
+  templateUrl: './masthead-search.component.html',
+  styleUrls: ['./masthead-search.component.scss'],
+})
+export class MastheadSearchComponent {
+  // @Input() navList: IMastheadMenuItem[];
+  // public buttonType = ButtonTypeEnum;
+}
