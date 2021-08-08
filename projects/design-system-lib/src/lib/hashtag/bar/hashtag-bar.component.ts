338dee0b1a686aa934847bf4e0bf37b7713992f6
NatalyIvanova <letter4natalia@gmail.com>
2021-08-08 15:45:37 +0300

    pixel pushing

--- projects/design-system-lib/src/lib/hashtag/bar/hashtag-bar.component.ts
+++ projects/design-system-lib/src/lib/hashtag/bar/hashtag-bar.component.ts
@@ -1,6 +1,5 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {IHashtag} from '../hashtag.interface';
-import {ButtonTypeEnum} from '../../button/button-type.enum';
 
 @Component({
   selector: 'p-hashtag-bar',
@@ -9,41 +8,6 @@ import {ButtonTypeEnum} from '../../button/button-type.enum';
 })
 export class HashtagBarComponent {
   @Input() hashtagList: IHashtag[];
-  @Input() menuList = [
-    { name: 'Alabama',
-      disabled: false
-    },
-    { name: 'Alaska disabled',
-      disabled: true
-    },
-    { name: 'California',
-      disabled: false
-    }
-  ];
-
-  @Input() footerBtnFirst = {
-    label: 'Done',
-    buttonType: ButtonTypeEnum.Primary
-  };
-  @Input() footerBtnLast = {
-    label: 'Cancel',
-    buttonType: ButtonTypeEnum.Secondary
-  };
-  @Input() filterAriaLabel = 'Find Label';
-  @Input() filterPlaceholder = 'Find Label';
-
-  @Input() headerBtnFirst = {
-    label: 'Clear selected',
-    buttonType: ButtonTypeEnum.Tertiary,
-    icon: {
-      iconName: 'eraser',
-      width: '15',
-      height: '15'
-    }
-  };
-  @Input() headerBtnLast = {
-    label: 'Not equal to'
-  };
 
   removeHashtag(event, hashtag): void {
     event.stopPropagation();
