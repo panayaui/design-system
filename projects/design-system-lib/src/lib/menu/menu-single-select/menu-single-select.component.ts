2ef0a4459426cb8af009383dfb4bc1ea0552ca5d
NatalyIvanova <letter4natalia@gmail.com>
2021-08-10 18:41:04 +0300

    menu-single-select: footer buttons in loop & footer height with CssCV

--- projects/design-system-lib/src/lib/menu/menu-single-select/menu-single-select.component.ts
+++ projects/design-system-lib/src/lib/menu/menu-single-select/menu-single-select.component.ts
@@ -14,8 +14,7 @@ export class MenuSingleSelectComponent implements OnInit, OnDestroy {
   @Input() groupTitle: boolean = false; // true if there is a group title
   @Input() filterPlaceholder: string; // if there is a search and buttons
   @Input() filterAriaLabel: string;
-  @Input() buttonFirst: IMenuButton;
-  @Input() buttonLast: IMenuButton;
+  @Input() footerButtons: IMenuButton[];
   @Output() onClickTask: EventEmitter<any> = new EventEmitter();
   @ViewChild('menuTrigger') menuTrigger: MatMenuTrigger;
   public filteredList: any[];
