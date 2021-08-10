1c6ec41136f6d62589e7e9dd2c57c45e4fb04990
NatalyIvanova <letter4natalia@gmail.com>
2021-08-10 10:48:32 +0300

    icon component back to bg image for custom icons

--- projects/design-system-lib/src/lib/icon/icon.component.ts
+++ projects/design-system-lib/src/lib/icon/icon.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { allIcons } from 'angular-feather/icons';
-import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'p-icon',
@@ -31,11 +30,9 @@ export class IconComponent implements OnInit {
     };
   }
 
-  public get customName(): SafeUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(this.iconName + '.svg');
+  public get customName(): string {
+    return this.disabled ? `p-icon-custom-${this.iconName}-disabled` : `p-icon-custom-${this.iconName}`;
   }
-
-  constructor(protected sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     allIcons[this.capitalizeName()] ? this.customIcon = false : this.customIcon = true;
